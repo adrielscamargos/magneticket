115dc0f015dccbf80ab994aaef79316419a5b192
Adriel Silva Camargos <34457423+adrielscamargos@users.noreply.github.com>
2023-06-09 18:40:22 -0300

    ok
    ok

--- lib/x86_64-linux-gnu/cmake/codec2/Codec2Config.cmake
+++
@@ -1,8 +0,0 @@
-get_filename_component(CODEC2_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
-if(NOT TARGET Codec2::codec2)
-  include("${CODEC2_CMAKE_DIR}/Codec2Targets.cmake")
-endif()
-
-# Backwards compatability
-set(Codec2_LIBRARIES Codec2::codec2)
